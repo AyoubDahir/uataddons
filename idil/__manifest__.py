bbf7c98883c751f07d5d628a7369e27620b518da
ayuubi <ayoubdahir10@gmail.com>
2025-11-22 12:48:16 +0300

    show dashboard behind purhase management

--- idil/__manifest__.py
+++ idil/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "BizCore",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "category": "Core Business Platform",
     "license": "LGPL-3",
     "summary": " Core Business Platform",
