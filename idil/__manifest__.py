3a676ef5093d726daec4864429bbc3c49f9ae4d4
Ayoub Dahir ali <69686810+AyoubDahir@users.noreply.github.com>
2025-12-20 12:03:57 +0300

    Update __manifest__.py
    test1

--- idil/__manifest__.py
+++ idil/__manifest__.py
@@ -8,7 +8,7 @@
     "depends": ["mail", "point_of_sale", "web"],
     "application": True,
     "sequence": -100,
-    "author": "ml",
+    "author": "m",
     "assets": {
         "web.assets_common": [
         ],
